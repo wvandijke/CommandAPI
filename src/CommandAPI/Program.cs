9749a39b97e828416552c4d748443e6bc7e35e42
Willem van Dijke <wavandijke@syntess.nl>
2023-10-05 15:40:44 +0200

    Initial Commit

---
+++ src/CommandAPI/Program.cs
@@ -0,0 +1,16 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        
+        var app = builder.Build();
+
+        //app.MapGet("/", () => "Hello World!");
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
